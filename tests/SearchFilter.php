70ef1cc7983789dca46777a20a0ca533b6c4bcf3
nebo <nebojsa.kamber@4zida.rs>
2023-03-17 12:27:23 +0100

    Sort properties and test methods accordingly

--- tests/SearchFilter.php
+++ tests/SearchFilter.php
@@ -9,6 +9,42 @@ use Nebkam\OdmSearchParam\SearchParamType;
 
 class SearchFilter
 	{
+	#[SearchParam(type: SearchParamType::String, field: 'alias')]
+	public ?string $aliasProperty = null;
+
+	#[SearchParam(type: SearchParamType::Bool)]
+	public ?bool $boolProperty = null;
+
+	#[SearchParam(callable: [SearchFilterDependency::class, 'setFoo'])]
+	public ?int $callableProperty = null;
+
+	#[SearchParam(type: SearchParamType::Exists)]
+	public ?string $existsProperty = null;
+
+	#[SearchParam(type: SearchParamType::Int)]
+	public ?string $intProperty = null;
+
+	#[SearchParam(type: SearchParamType::IntArray)]
+	public ?array $intArrayProperty = null;
+
+	#[SearchParam(type: SearchParamType::IntArray, invert: true)]
+	public ?array $intArrayInvertedProperty = null;
+
+	#[SearchParam(type: SearchParamType::Range, direction: SearchParamDirection::From)]
+	public ?DateTime $rangeDateTimeFromProperty = null;
+
+	#[SearchParam(type: SearchParamType::RangeFloat, direction: SearchParamDirection::From)]
+	public ?string $rangeFloatFromProperty = null;
+
+	#[SearchParam(type: SearchParamType::RangeFloat, direction: SearchParamDirection::To)]
+	public ?string $rangeFloatToProperty = null;
+
+	#[SearchParam(type: SearchParamType::RangeInt, direction: SearchParamDirection::From)]
+	public ?string $rangeIntFromProperty = null;
+
+	#[SearchParam(type: SearchParamType::RangeInt, direction: SearchParamDirection::To)]
+	public ?string $rangeIntToProperty = null;
+
 	#[SearchParam(type: SearchParamType::String)]
 	public ?string $stringProperty = null;
 
@@ -27,42 +63,6 @@ class SearchFilter
 	#[SearchParam(type: SearchParamType::StringEnumArray)]
 	public ?array $stringEnumArrayProperty = null;
 
-	#[SearchParam(type: SearchParamType::Int)]
-	public ?string $intProperty = null;
-
-	#[SearchParam(type: SearchParamType::Bool)]
-	public ?bool $boolProperty = null;
-
 	#[SearchParam(type: SearchParamType::VirtualBool)]
 	public ?bool $virtualBoolProperty = null;
-
-	#[SearchParam(type: SearchParamType::Exists)]
-	public ?string $existsProperty = null;
-
-	#[SearchParam(type: SearchParamType::Range, direction: SearchParamDirection::From)]
-	public ?DateTime $rangeDateTimeFromProperty = null;
-
-	#[SearchParam(type: SearchParamType::RangeInt, direction: SearchParamDirection::From)]
-	public ?string $rangeIntFromProperty = null;
-
-	#[SearchParam(type: SearchParamType::RangeInt, direction: SearchParamDirection::To)]
-	public ?string $rangeIntToProperty = null;
-
-	#[SearchParam(type: SearchParamType::RangeFloat, direction: SearchParamDirection::From)]
-	public ?string $rangeFloatFromProperty = null;
-
-	#[SearchParam(type: SearchParamType::RangeFloat, direction: SearchParamDirection::To)]
-	public ?string $rangeFloatToProperty = null;
-
-	#[SearchParam(type: SearchParamType::IntArray)]
-	public ?array $integerArrayProperty = null;
-
-	#[SearchParam(type: SearchParamType::IntArray, invert: true)]
-	public ?array $integerArrayInvertedProperty = null;
-
-	#[SearchParam(type: SearchParamType::String, field: 'alias')]
-	public ?string $aliasProperty = null;
-
-	#[SearchParam(callable: [SearchFilterDependency::class, 'setFoo'])]
-	public ?int $callableProperty = null;
 	}
