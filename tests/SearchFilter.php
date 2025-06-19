2c47af0c26f3961e8eb2ee9e99684c9a5b5dee5f
nebo <nebojsa.kamber@4zida.rs>
2025-06-19 13:19:30 +0200

    Sort properties alphabetically

--- tests/SearchFilter.php
+++ tests/SearchFilter.php
@@ -18,20 +18,17 @@ class SearchFilter
 	#[SearchParam(type: SearchParamType::Callable, callable: [SearchFilterDependency::class, 'setFoo'])]
 	public ?int $callableProperty = null;
 
-	#[SearchParam(type: SearchParamType::Exists)]
-	public ?string $existsProperty = null;
-
 	#[SearchParam(type: SearchParamType::Exists, invert: true)]
 	public ?string $existsInvertedProperty = null;
 
-	#[SearchParam(type: SearchParamType::Int)]
-	public ?string $intProperty = null;
+	#[SearchParam(type: SearchParamType::Exists)]
+	public ?string $existsProperty = null;
+
+	#[SearchParam(type: SearchParamType::IntArray, invert: true)]
+	public ?array $intArrayInvertedProperty = null;
 
 	#[SearchParam(type: SearchParamType::IntArray)]
 	public ?array $intArrayProperty = null;
-
-	#[SearchParam(type: SearchParamType::IntEnum)]
-	public ?IntEnum $intEnumProperty = null;
 
 	/**
 	 * @var IntEnum[]|null
@@ -39,8 +36,17 @@ class SearchFilter
 	#[SearchParam(type: SearchParamType::IntEnumArray)]
 	public ?array $intEnumArrayProperty = null;
 
-	#[SearchParam(type: SearchParamType::IntArray, invert: true)]
-	public ?array $intArrayInvertedProperty = null;
+	#[SearchParam(type: SearchParamType::IntEnum)]
+	public ?IntEnum $intEnumProperty = null;
+
+	#[SearchParam(type: SearchParamType::Int)]
+	public ?string $intProperty = null;
+
+	/**
+	 * @var float[]|null
+	 */
+	#[SearchParam(type: SearchParamType::GeoWithinBox)]
+	public ?array $mapBoundary = null;
 
 	#[SearchParam(type: SearchParamType::Range, direction: SearchParamDirection::From)]
 	public ?DateTime $rangeDateTimeFromProperty = null;
@@ -51,32 +57,26 @@ class SearchFilter
 	#[SearchParam(type: SearchParamType::RangeFloat, direction: SearchParamDirection::To)]
 	public ?string $rangeFloatToProperty = null;
 
-	#[SearchParam(type: SearchParamType::RangeInt, direction: SearchParamDirection::From)]
-	public ?string $rangeIntFromProperty = null;
-
-	#[SearchParam(type: SearchParamType::RangeInt, direction: SearchParamDirection::To)]
-	public ?string $rangeIntToProperty = null;
-
 	#[SearchParam(type: SearchParamType::RangeIntEnum, direction: SearchParamDirection::From)]
 	public ?IntEnum $rangeIntEnumFromProperty = null;
 
 	#[SearchParam(type: SearchParamType::RangeIntEnum, direction: SearchParamDirection::To)]
 	public ?IntEnum $rangeIntEnumToProperty = null;
 
+	#[SearchParam(type: SearchParamType::RangeInt, direction: SearchParamDirection::From)]
+	public ?string $rangeIntFromProperty = null;
+
+	#[SearchParam(type: SearchParamType::RangeInt, direction: SearchParamDirection::To)]
+	public ?string $rangeIntToProperty = null;
+
 	#[SearchParam(type: SearchParamType::Regex, flags: 'i')]
 	public ?string $regexProperty = null;
-
-	#[SearchParam(type: SearchParamType::String)]
-	public ?string $stringProperty = null;
-
-	#[SearchParam(type: SearchParamType::StringArray)]
-	public ?array $stringArrayProperty = null;
 
 	#[SearchParam(type: SearchParamType::StringArray, invert: true)]
 	public ?array $stringArrayInvertedProperty = null;
 
-	#[SearchParam(type: SearchParamType::StringEnum)]
-	public ?StringEnum $stringEnumProperty = null;
+	#[SearchParam(type: SearchParamType::StringArray)]
+	public ?array $stringArrayProperty = null;
 
 	/**
 	 * @var StringEnum[]|null
@@ -84,14 +84,14 @@ class SearchFilter
 	#[SearchParam(type: SearchParamType::StringEnumArray)]
 	public ?array $stringEnumArrayProperty = null;
 
+	#[SearchParam(type: SearchParamType::StringEnum)]
+	public ?StringEnum $stringEnumProperty = null;
+
+	#[SearchParam(type: SearchParamType::String)]
+	public ?string $stringProperty = null;
+
 	#[SearchParam(type: SearchParamType::VirtualBool)]
 	public ?bool $virtualBoolProperty = null;
-
-	/**
-	 * @var float[]|null
-	 */
-	#[SearchParam(type: SearchParamType::GeoWithinBox)]
-	public ?array $mapBoundary = null;
 
 	#[SearchParam(type: SearchParamType::GeoWithinBox)]
 	public ?string $wrongMapBoundary = null;
